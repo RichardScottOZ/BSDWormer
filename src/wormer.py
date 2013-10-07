5e8df9a8d9ff500d3e07e8b25f29a8a0a9ba55ac
Frank Horowitz <frank@horow.net>
2013-10-06 23:23:40 -0400

    Added some more gdal stuff

--- src/wormer.py
+++ src/wormer.py
@@ -1,5 +1,11 @@
 import numpy as np
 import scipy as sp
+from osgeo import gdalnumeric
+from osgeo import gdal
+#from osgeo import ogr
+#from osgeo import osr
+#from osgeo import gdal_array
+from osgeo import gdalconst
 
 class Wormer(object):
     """Class to be overview of whole worming method
@@ -23,13 +29,15 @@ class Wormer(object):
         self.base_grid= grid
         
     def importGdalRaster(self,gdal_filename):
-        """Setter for base_grid
+        """Import a GDAL raster into a numpy array
         >>> foo = Wormer()
         >>> filename = '../test_data/SuartBasin/suart_basin.ers'
         >>> foo.importGdalRaster(filename)
         >>> assert foo.gdal_input_filename == filename
+        >>> assert foo.base_grid.shape == (1440,960)
         """
         self.gdal_input_filename = gdal_filename
+        self.setBaseGrid(np.array(gdalnumeric.LoadFile(self.gdal_input_filename))
 
     
 if __name__ == "__main__":
