6d2ad6e85fb29ab7ee76d32f5dfdc071ebf3476d
Frank Horowitz <frank@horow.net>
2013-10-06 23:48:31 -0400

    Tests passgit add wormer.pygit add wormer.py (But filename harwired in incorrectly...)

--- src/wormer.py
+++ src/wormer.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy as sp
+#import scipy as sp
 from osgeo import gdalnumeric
 from osgeo import gdal
 #from osgeo import ogr
@@ -31,15 +31,16 @@ class Wormer(object):
     def importGdalRaster(self,gdal_filename):
         """Import a GDAL raster into a numpy array
         >>> foo = Wormer()
-        >>> filename = '../test_data/SuartBasin/suart_basin.ers'
+        >>> filename = '/Users/frank/Documents/Src/Git Stuff/BSDWormer/test_data/SuartBasin/suart_basin.ers'
         >>> foo.importGdalRaster(filename)
         >>> assert foo.gdal_input_filename == filename
         >>> assert foo.base_grid.shape == (1440,960)
         """
         self.gdal_input_filename = gdal_filename
-        self.setBaseGrid(np.array(gdalnumeric.LoadFile(self.gdal_input_filename))
+        self.setBaseGrid(np.array(gdalnumeric.LoadFile(self.gdal_input_filename)))
 
     
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     import doctest
     doctest.testmod()
